930cfb707737efa17fc87ae8ac7a88ec76f588fd
associate2coder <olatsko@gmail.com>
2025-02-10 13:07:11 +0200

    fix url for api client

--- src/shared/utils/apiClient.ts
+++ src/shared/utils/apiClient.ts
@@ -1,4 +1,4 @@
-const BASE_URL = `${window.location.origin}/`;
+const BASE_URL = `${window.location.origin}${window.location.pathname}`;
 
 type RequestMethod = 'GET' | 'POST' | 'PATCH' | 'DELETE';
 
